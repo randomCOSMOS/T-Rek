7db556a6176fcf7f682fe39beed8f818d073d1dd
COSMOS <srijanwhat2@gmail.com>
2024-03-06 22:46:23 +0530

    Connected to firebase and tested

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os
 from dotenv import load_dotenv
-from discord import Intents, Client, Message
-from responses import get_response 
+from discord import Intents, Client, Message, AllowedMentions
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
+cred = credentials.Certificate('./t-rek-cfafd-firebase-adminsdk-651wx-a3fc8cbe5f.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
+# doc_ref = db.collection('logs').document()
+# doc_ref.set()
+
+# 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -11,21 +20,6 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 intents = Intents.default()
 intents.message_content = True
 client = Client(intents=intents)
-
-
-async def send_message(message, user_message):
-    if not user_message:
-        print("fucked up")
-        return
-    
-    if is_private := user_message[0] == "?":
-        user_message = user_message[1:]
-
-    try:
-        responses = get_response(user_message)
-        await message.author.send(responses) if is_private else await message.channel.send(responses)
-    except Exception as e:
-        print(e)
 
 
 @client.event
@@ -37,12 +31,16 @@ async def on_message(msg):
     if msg.author == client.user:
         return
     
-    username = msg.author
-    user_message = msg.content
-    channel = str(msg.channel)
+    if msg.content[0] == "=":
+        msg.content = msg.content[1:]
 
-    print(f"[{channel}] {username}: '{user_message}'")
-    await send_message(msg, user_message)
+        username = msg.author
+        user_message = msg.content
+        channel = str(msg.channel)
+
+        print(f"[{channel}] {username}: '{user_message}'")
+        await msg.channel.send(f"Got an message from {username.mention} that says \n `{user_message}`")
 
 
 client.run(TOKEN)
+
